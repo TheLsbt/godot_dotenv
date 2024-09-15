767b653b7415b297fe632aa1cc92b665bffb40fb
TheLsbt <tyresewolmarans@gmail.com>
2024-09-15 03:36:34 +0200

    Simplified the plugin script, and removed complements system

--- addons/dotenv/plugin.gd
+++ addons/dotenv/plugin.gd
@@ -1,59 +1,23 @@
 @tool
 extends EditorPlugin
 
-const setting_path = 'docks/filesystem/textfile_extensions'
-
-# Roadmap:
-# Syntax highlighting
-# Complements system
-#	Load complements
-# Add Settings for plugin
-
-# Code parsing and error throwing in the editor
-
+const SyntaxHighlighting = preload("res://addons/dotenv/complements/syntax_highlighting.gd")
+const SETTING_PATH = 'docks/filesystem/textfile_extensions'
 
 var original_setting = ''
-var active_complements = []
+var syntax_highlighting: SyntaxHighlighting
 
 
 func _enter_tree() -> void:
-	# We do this to recognise .env files
-	# Would like if there is another way that doesnt modifying the editor settings
+	# To recognise .env file we need to modify a option in the editor settings
 	var settings = EditorInterface.get_editor_settings()
-	original_setting = settings.get_setting(setting_path)
-	settings.set_setting(setting_path, original_setting + ',env')
+	if not (settings.get_setting(SETTING_PATH) as String).contains('env'):
+		settings.set_setting(SETTING_PATH, original_setting + ',env')
 
-	load_complements()
+	syntax_highlighting = SyntaxHighlighting.new()
+	syntax_highlighting.onload()
 
 
 func _exit_tree() -> void:
-	var settings = EditorInterface.get_editor_settings()
-	settings.set_setting(setting_path, original_setting)
-
-	unload_complements()
-
-
-func load_complements() -> void:
-	var script = get_script()
-	var path = ''
-
-	if script is Script:
-		path = script.resource_path.get_base_dir() + '/complements'
-
-	if not DirAccess.dir_exists_absolute(path):
-		printerr('Failed to find the complements directory')
-		return
-
-	var files = DirAccess.get_files_at(path)
-	for file in files:
-		# NOTE: There is no type checking for a complement so technically any script can be one
-		# but i might add this in the future, to catch errors
-		var complement = load(path + '/' + file).new()
-		complement.onload()
-		print('Loaded ', complement._get_complement_name())
-		active_complements.append(complement)
-
-
-func unload_complements() -> void:
-	for complement in active_complements:
-		complement.unload()
+	if syntax_highlighting:
+		syntax_highlighting.unload()
