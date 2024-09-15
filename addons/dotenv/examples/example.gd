aa2695dfac2d31a4b66f101bb5ee363044c97fe2
TheLsbt <tyresewolmarans@gmail.com>
2024-09-15 09:00:47 +0200

    Qol features and fixes
    Added the ability to recursively
    Fixed the load to load_ to not override the built in function

--- addons/dotenv/examples/example.gd
+++ addons/dotenv/examples/example.gd
@@ -2,7 +2,7 @@ extends Node
 
 
 func _ready() -> void:
-	Dotenv.load('res://addons/dotenv/examples/example.env')
+	Dotenv.load_('res://addons/dotenv/examples/example.env', false, false)
 	print('username = ', OS.get_environment('USERNAME'))
 	print('password = ', OS.get_environment('PASSWORD'))
 	print('lucky number = ', OS.get_environment('LUCKY_NUMBER'))
