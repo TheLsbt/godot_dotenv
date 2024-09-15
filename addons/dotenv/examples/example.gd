d980835a663da5e1afb29290ff7c5b5eaeb7613e
TheLsbt <tyresewolmarans@gmail.com>
2024-09-15 04:50:08 +0200

    Added multiple items
    Changed example.gd and example.env to reflect the new assets.
    Added assets

--- addons/dotenv/examples/example.gd
+++ addons/dotenv/examples/example.gd
@@ -3,4 +3,6 @@ extends Node
 
 func _ready() -> void:
 	Dotenv.load('res://addons/dotenv/examples/example.env')
-	print(OS.get_environment('PASSWORD'))
+	print('username = ', OS.get_environment('USERNAME'))
+	print('password = ', OS.get_environment('PASSWORD'))
+	print('lucky number = ', OS.get_environment('LUCKY_NUMBER'))
